d795f8dfbb1345998c7197423734f6ba87945c0a
callumflack <749885+callumflack@users.noreply.github.com>
2026-03-04 14:29:26 +0000

    ci(connect): sync DataConnect download version

--- connect/src/config/config.ts
+++ connect/src/config/config.ts
@@ -8,7 +8,7 @@ export type DownloadAsset = {
   comingSoon?: boolean;
 };
 
-const DOWNLOAD_VERSION = "0.7.29";
+const DOWNLOAD_VERSION = "0.7.30";
 const DATA_CONNECT_GITHUB_RELEASES_URL =
   "https://github.com/vana-com/data-connect/releases";
 
