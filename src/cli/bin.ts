ee856817fdc35d76c7446e99be265e583726ad2d
Tim Nunamaker <tnunamak@gmail.com>
2026-03-14 07:35:53 -0500

    fix(cli): ignore epipe from piped output

--- src/cli/bin.ts
+++ src/cli/bin.ts
@@ -2,6 +2,15 @@
 
 import { runCli } from "./index.js";
 
+for (const stream of [process.stdout, process.stderr]) {
+  stream.on("error", (error: NodeJS.ErrnoException) => {
+    if (error.code === "EPIPE") {
+      process.exit(0);
+    }
+    throw error;
+  });
+}
+
 const exitCode = await runCli(process.argv);
 if (typeof exitCode === "number") {
   process.exitCode = exitCode;
